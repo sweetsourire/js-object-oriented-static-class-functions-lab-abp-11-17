09d1aebda37705f7b03e087efc44acfcdab5c072
Bomi Kim <bkim1289@gmail.com>
2017-11-15 17:27:08 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,20 @@
-'use strict';
+class Question {
+  constructor(content) {
+    this.content = content;
+    this.save()
+  }
+
+  static All() {
+    return this._All;
+  }
+
+  static Find(id) {
+    return this.All()[id-1]
+  }
+
+  save(){
+    this.constructor._All.push(this);
+  }
+}
+
+Question._All = [];
